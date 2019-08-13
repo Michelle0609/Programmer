7c9ea6f920f0039b6334bfc859e3e49ca2a4f208
Michelle <bibili0609@gmail.com>
2019-08-13 21:33:19 +1000

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Programmer
+{
+    public List<string> Languages = new List<string>();
+     
+    public void AddLanguage(string language)
+    {
+        if(!Languages.Contains(language))
+        {
+            Languages.Add(language);
+        }
+       
+    }
+
+}
+
+public class ProgrammerTeacher : Programmer
+{
+    public bool Teach(Programmer programmer,string language)
+    {
+        if(Languages.Contains(language))
+        {
+            programmer.Languages.Add(language);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ProgrammerTeacher teacher = new ProgrammerTeacher();
+        teacher.AddLanguage("C#");
+
+        Programmer programmer = new Programmer();
+        teacher.Teach(programmer, "C#");
+
+        foreach (var language in programmer.Languages)
+            Console.WriteLine(language);
+    }
+
+}
+
